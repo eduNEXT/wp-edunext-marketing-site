4907e52a99ff3cb81a6ddbf71eadac04c387a551
Squirrel18 <daniel.quiroga@edunext.co>
2019-03-29 12:07:55 -0500

    Help box changes.

--- includes/templates/shortcode-course-details-help-meta-box.php
+++ includes/templates/shortcode-course-details-help-meta-box.php
@@ -1,13 +1,17 @@
-<p>Example of usage:<br>
+<p>Example of usage:
 <pre>
-[course_details course_id="edX+CS105" course_details="video, title, short_description, start" course_title_styles="style1, style2" course_video_styles="my_custom_video_style"]
+[course_details
+    course_id="edX+DemoX"
+    course_details="title, short_description, start, video"
+    course_title_styles="my-custom-title-style-1, my-custom-title-style-2"
+    course_video_styles="my-custom-video-style"]
 </pre>
 <p>Shortcode attributes:</p>
-<ul style="list-style-type: square;">
-    <li>course_id - Required</li><p>Open edX Discovery API course id, it must include the short organization name and course number. e.g edX+DemoX</p>
-    <li>course_details - Required</li><p>String or list separated by commas, with desired course details. (title, short_description, start, video)</p>
-    <li>course_title_styles - Optional</li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
-    <li>course_short_description_styles - Optional</li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
-    <li>course_start_styles - Optional</li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
-    <li>course_video_styles - Optional</li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
+<ul>
+    <li><strong>course_id - Required</strong></li><p>Open edX Discovery API course id, it must include the short organization name and course number. e.g edX+DemoX</p>
+    <li><strong>course_details - Required</strong></li><p>String or list separated by commas, with desired course details. (title, short_description, start, video)</p>
+    <li><strong>course_title_styles - Optional</strong></li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
+    <li><strong>course_short_description_styles - Optional</strong></li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
+    <li><strong>course_start_styles - Optional</strong></li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
+    <li><strong>course_video_styles - Optional</strong></li><p>String or list separated by commas, with desired class names. e.g my-awesome-style</p>
 </ul>
