fcdbe4b660d06d5163b8b7ef76b4b2f121f751b5
Felipe Montoya <felipe.montoya@edunext.co>
2019-05-28 15:45:46 -0500

    Adding empty enrollments class

---
+++ includes/class-wp-openedx-enrollment.php
@@ -0,0 +1,37 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WP_Openedx_Enrollment {
+
+	/**
+	 * The main plugin object.
+	 * @var 	object
+	 * @access  public
+	 * @since 	1.0.0
+	 */
+	public $parent = null;
+
+	public function __construct ( $parent ) {
+		$this->parent = $parent;
+	}
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->parent->_version );
+	} // End __clone()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->parent->_version );
+	} // End __wakeup()
+
+}
