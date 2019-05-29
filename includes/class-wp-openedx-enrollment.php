704287a8868c7e58fd296c68ad8ca1f5a7d07b0d
Felipe Montoya <felipe.montoya@edunext.co>
2019-05-29 15:20:57 -0500

    Convert to enrollment request with custom actions

--- includes/class-wp-openedx-enrollment.php
+++ includes/class-wp-openedx-enrollment.php
@@ -36,10 +36,10 @@ class WP_Openedx_Enrollment {
 			'show_ui' => true,
 			'show_in_menu' => false,
 			'show_in_nav_menus' => true,
-			'supports' => array( 'title', 'revisions'),
+			'supports' => array(''),
 			'menu_icon' => 'dashicons-admin-post'
 		);
-		$this->parent->register_post_type('openedx_enrollment', 'Open edX Enrollments', 'Open edX Enrollment', '', $enrollment_cpt_options);
+		$this->parent->register_post_type('openedx_enrollment', 'Open edX Enrollment Requests', 'Open edX Enrollment Request', '', $enrollment_cpt_options);
 
 		// Register the CPT actions
 		add_action( 'save_post', array( $this, 'save'), 10, 3 );
@@ -58,28 +58,32 @@ class WP_Openedx_Enrollment {
 
 		// Update the $post metadata
 
-		update_post_meta( $post_id, 'course_id', sanitize_text_field( $_POST['oe_course_id'] ) );
-		update_post_meta( $post_id, 'bundle_id', sanitize_text_field( $_POST['oe_bundle_id'] ) );
-		update_post_meta( $post_id, 'email', sanitize_text_field( $_POST['oe_email'] ) );
-		update_post_meta( $post_id, 'username', sanitize_text_field( $_POST['oe_username'] ) );
-		update_post_meta( $post_id, 'mode', sanitize_text_field( $_POST['oe_mode'] ) );
+		update_post_meta( $post_id, 'course_id', sanitize_text_field( $_POST['oer_course_id'] ) );
+		update_post_meta( $post_id, 'bundle_id', sanitize_text_field( $_POST['oer_bundle_id'] ) );
+		update_post_meta( $post_id, 'email', sanitize_text_field( $_POST['oer_email'] ) );
+		update_post_meta( $post_id, 'username', sanitize_text_field( $_POST['oer_username'] ) );
+		update_post_meta( $post_id, 'mode', sanitize_text_field( $_POST['oer_mode'] ) );
 
-		// Cant deal with bools right now
+		if ('oer_process' == $_POST['oer_action']) {
+			$this->process_request($post_id, $post, false);
+		}
+		if ('oer_force' == $_POST['oer_action']) {
+			$this->process_request($post_id, $post, true);
+		}
+		if ('oer_sync' == $_POST['oer_action']) {
+			// Do the Sync
+		}
+	}
 
-		// if ( isset( $_POST['oe_is_active'] ) ) {
-		// 	update_post_meta( $post_id, 'is_active', TRUE );
-		// } else {
-		// 	update_post_meta( $post_id, 'is_active', FALSE );
-		// }
-
-		// if ( isset( $_POST['oe_force'] ) ) {
-		// 	update_post_meta( $post_id, 'force', TRUE );
-		// } else {
-		// 	update_post_meta( $post_id, 'force', FALSE );
-		// }
-
-		// Here we can connect the API update logic
-
+	/**
+	 * Save post metadata when a post is saved.
+	 *
+	 * @param int $post_id The post ID.
+	 * @param post $post The post object.
+	 * @param bool $force Does this order need procesing by force?
+	 */
+	function process_request( $post_id, $post, $force ) {
+		// Do something
 	}
 
 	/**
@@ -90,7 +94,46 @@ class WP_Openedx_Enrollment {
 	function set_up_admin() {
 
 		// Extra info
-		add_action( 'edit_form_after_title', array( $this, 'edit_form_after_title' ) );
+		add_action( 'edit_form_after_title', array( $this, 'render_enrollment_info_form' ) );
+
+		add_action( 'add_meta_boxes' , array( $this, 'replace_admin_meta_boxes' ) );
+	}
+
+	/**
+	 * @return void
+	 */
+	function replace_admin_meta_boxes() {
+		remove_meta_box( 'submitdiv', $this->post_type, 'side' );
+
+		add_meta_box( 'openedx_enrollment_request_actions', sprintf( __( '%s actions', '' ), 'Open edX Enrollment Requests' ), array( $this, 'render_actions_box' ), $this->post_type, 'side', 'high' );
+	}
+
+	/**
+	 * Renders the actions box for the edit post box
+	 *
+	 * @return void
+	 */
+	public function render_actions_box() {
+		?>
+		<ul class="enrollment_actions submitbox">
+
+			<li class="wide" id="actions">
+				<select name="oer_action">
+					<option value=""><?php esc_html_e( 'Choose an action...', 'wp-edunext-marketing-site' ); ?></option>
+					<option value="oer_process"><?php esc_html_e( 'Process', 'wp-edunext-marketing-site' ); ?></option>
+					<option value="oer_force"><?php esc_html_e( 'Process --force', 'wp-edunext-marketing-site' ); ?></option>
+					<option value="oer_sync"><?php esc_html_e( 'Synchronize (pull)', 'wp-edunext-marketing-site' ); ?></option>
+				</select>
+				<button class="button wc-reload"><span><?php esc_html_e( 'Apply', 'wp-edunext-marketing-site' ); ?></span></button>
+			</li>
+
+			<li class="wide">
+				<button type="submit" class="button save_order button-primary" name="save" value="save_no_process"><?php esc_html_e( 'Save without processing', 'wp-edunext-marketing-site' ); ?>
+				</button>
+			</li>
+
+		</ul>
+		<?php
 	}
 
 	/**
@@ -98,66 +141,58 @@ class WP_Openedx_Enrollment {
 	 *
 	 * @param WP_Post $post Current post object.
 	 */
-	public function edit_form_after_title( $post ) {
+	public function render_enrollment_info_form( $post ) {
 
 		if ( $this->post_type != $post->post_type ) return;
 		$post_id = $post->ID;
 
 		?>
 		<div id="namediv" class="postbox">
-		<h2 class="">Open edX enrollment information</h2>
+		<h2 class="">Open edX enrollment request</h2>
 		<fieldset>
 		<table class="form-table">
 			<tbody>
 				<tr>
 					<td class="first"><label for="openedx_enrollment_course_id">course_id</label></td>
 					<td>
-						<input type="text" id="openedx_enrollment_course_id" name="oe_course_id"
+						<input type="text" id="openedx_enrollment_course_id" name="oer_course_id"
 						value="<?php echo(get_post_meta($post_id, 'course_id', true)); ?>">
 					</td>
 				</tr>
 				<tr>
 					<td class="first"><label for="openedx_enrollment_bundle_id">bundle_id</label></td>
 					<td>
-						<input type="text" id="openedx_enrollment_bundle_id" name="oe_bundle_id"
+						<input type="text" id="openedx_enrollment_bundle_id" name="oer_bundle_id"
 						value="<?php echo(get_post_meta($post_id, 'bundle_id', true)); ?>">
 					</td>
 				</tr>
 				<tr>
 					<td class="first"><label for="openedx_enrollment_email">email</label></td>
 					<td>
-						<input type="email" id="openedx_enrollment_email" name="oe_email"
+						<input type="email" id="openedx_enrollment_email" name="oer_email"
 						value="<?php echo(get_post_meta($post_id, 'email', true)); ?>">
 					</td>
 				</tr>
 				<tr>
 					<td class="first"><label for="openedx_enrollment_username">username</label></td>
 					<td>
-						<input type="text" id="openedx_enrollment_username" name="oe_username"
+						<input type="text" id="openedx_enrollment_username" name="oer_username"
 						value="<?php echo(get_post_meta($post_id, 'username', true)); ?>">
 					</td>
 				</tr>
 				<tr>
 					<td class="first"><label for="openedx_enrollment_mode">mode</label></td>
 					<td>
-						<input type="text" id="openedx_enrollment_mode" name="oe_mode"
+						<input type="text" id="openedx_enrollment_mode" name="oer_mode"
 						value="<?php echo(get_post_meta($post_id, 'mode', true)); ?>">
 					</td>
 				</tr>
-				<!--
 				<tr>
 					<td class="first"><label for="openedx_enrollment_is_active">is_active</label></td>
 					<td>
-						<input type="checkbox" id="openedx_enrollment_is_active" name="oe_is_active" style="width: auto;">
+						<input type="checkbox" id="openedx_enrollment_is_active" name="oer_is_active" style="width: auto;">
 					</td>
 				</tr>
-				<tr>
-					<td class="first"><label for="openedx_enrollment_force">force</label></td>
-					<td>
-						<input type="checkbox" id="openedx_enrollment_force" name="oe_force" style="width: auto;">
-					</td>
-				</tr>
-				 -->
 			</tbody>
 		</table>
 		</fieldset>
