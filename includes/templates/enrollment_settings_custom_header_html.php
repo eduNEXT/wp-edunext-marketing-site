95f5967f33b38c4b6fd700751c0849ccbf24719b
Felipe Montoya <felipe.montoya@edunext.co>
2019-05-30 12:02:11 -0500

    Typos and spelling

--- includes/templates/enrollment_settings_custom_header_html.php
+++ includes/templates/enrollment_settings_custom_header_html.php
@@ -2,5 +2,5 @@
 <p><?= __( 'In a Wordpress - Open edX integrated scenario, there will be WP pages or posts that describe each of the courses, and in fact replace the course description page that Open edX provides.', 'wp-edunext-marketing-site' ) ?></p>
 <p><?= __( 'These WP pages should have a way to link directly into the corresponding course in the Open edX, but this should not be simple links, as they need to be aware of the current course settings and state of enrollment of the particular user in the course.' ) ?></p>
 <p><?= __( 'The idea of the <strong>Course buttons</strong> is to add to your Wordpress Pages a smart component that provides this connection.', 'wp-edunext-marketing-site' ) ?></p>
-<p><?= __( 'The Course buttons are added directly in each page via a <strong>Shortcode</strong>, and in each Page editor you\'ll be able to find a help box with all configuration posibilities for those shortcodes.', 'wp-edunext-marketing-site' ) ?></p>
+<p><?= __( 'The Course buttons are added directly in each page via a <strong>Shortcode</strong>, and in each Page editor you\'ll be able to find a help box with all configuration possibilities for those shortcodes.', 'wp-edunext-marketing-site' ) ?></p>
 <h2><?= __( 'Configurations for the Course buttons', 'wp-edunext-marketing-site' ) ?></h2>
--- includes/templates/navigation_settings_custom_header_html.php
+++ includes/templates/navigation_settings_custom_header_html.php
@@ -4,5 +4,5 @@
 
 <h2><?= __( 'Visit "Appearance -> Menus" and look for the "Open edX WP integrator" box on the left column to build the Open edX user menu for your WP site.', 'wp-edunext-marketing-site' ) ?></h2>
 
-<h4><?= __( 'Advanced Navigation Settins', 'wp-edunext-marketing-site' ) ?></h4>
+<h4><?= __( 'Advanced Navigation Settings', 'wp-edunext-marketing-site' ) ?></h4>
 <p><?= __( 'You normally don\'t need to make any changes here', 'wp-edunext-marketing-site' ) ?></p>
